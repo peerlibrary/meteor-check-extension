2f4ac0f4a623c698734e5e19de773f2c4e1a51c4
Mitar <mitar.git@tnode.com>
2016-05-13 09:46:12 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:check-extension',
   summary: "Extensions to the built-in check package",
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/peerlibrary/meteor-check-extension.git'
 });
 
