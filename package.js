d05ad31862207133beab5a735c1ad96179cdbfe1
Mitar <mitar.git@tnode.com>
2019-09-19 19:03:14 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'peerlibrary:check-extension',
   summary: "Extensions to the built-in check package",
-  version: '0.6.0',
+  version: '0.7.0',
   git: 'https://github.com/peerlibrary/meteor-check-extension.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'check',
     'underscore'
